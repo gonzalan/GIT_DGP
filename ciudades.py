04a1c2e7490a51b017d47d08784659e5382c112d
Antonio <Antonio@MacBook-Air.local>
2022-02-11 13:17:46 +0100

    Añadido un nuevo fichero python

---
+++ ciudades.py
@@ -0,0 +1,6 @@
+import pandas as pd
+ciudades = pd.Series({'Sevilla': 690000,
+                   'Valencia': 789004,
+                   'Granada': 233764,
+                   'Madrid': 3174000,
+                   'Barcelona': 1615000})
